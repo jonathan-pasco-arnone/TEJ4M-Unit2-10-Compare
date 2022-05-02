e603cfe34a766e9b9e91e2fd968306f97f3c8db6
jonathan-pasco-arnone <jonathan.pasco-arnone@mths.ca>
2022-05-02 15:05:30 +0100

    lower code

--- compare01.s
+++ compare01.s
@@ -6,16 +6,16 @@
 .text
 .global main
 main:
-    mov r1, #2147483648
-    mov r2, #-1
+    mov r1, #6
+    mov r2, #15
     cmp r1, r2 @ update cpsr condition codes with r1-r2
-    bvs overflow_underflow
+    blo lower
 
 case_different:
     mov r0, #2 @ r0 <- 2
     b end @ branch to end
 
-overflow_underflow:
+lower:
     mov r0, #1 @ r0 <- 1
 
 end:
